5bbeec5dc70ed8c4b04b6be0628a57f9ca1e7aa6
Jainam Zobaliya <56570177+JainamZobaliya@users.noreply.github.com>
2020-10-08 10:36:08 +0530

    Sidebar for Admin Pages

---
+++ adminSideBar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<script src='https://kit.fontawesome.com/a076d05399.js'></script>
+		<link rel="stylesheet" type="text/css" href="adminSideBar.css">
+        <title>Admin HomePage</title>
+    </head>
+    <body>
+        <div class="sidebar">
+			<ul class="sideNavLink">
+                <li class="sideNavBarLogo"><a class="logo" href="https://www.iplt20.com/"></a></li>
+                <li class="sideNavLinky"><a href="adminHomePage.php"class="activeTab"><i class="fa fa-home"></i> Home</a></li>
+                <li class="sideNavLinky"><a href="adminUserPage.php"><i class="fa fa-user"></i> User</a></li>
+                <li class="sideNavLinky"><a href="#"><i class="fa fa-shopping-bag"></i> Products</a></li>
+                <li class="sideNavLinky"><a href="#"><i class="fa fa-address-card"></i> Scores</a></li>
+                <li class="sideNavLinky"><a href="#"><i class="fa fa-newspaper-o"></i> News</a></li>
+				<li class="sideNavLinky"><a href="profile.php"><i class="fa fa-user-circle"></i> Profile</a></li>
+                <li class="sideNavLinky"><a href="logout.php"><i class="fa fa-power-off"></i> Logout</a></li>
+            </ul>                
+        </div>
+    </body>
+</html>
